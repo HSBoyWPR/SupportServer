b9ea7535f458f0fecc9065c10bec096b8fa519a1
HSBoyWPR <332107301@qq.com>
2018-11-20 23:24:42 +0800

    添加数据库数据、轨迹查询

--- Record/models.py
+++ Record/models.py
@@ -8,12 +8,22 @@ class User(models.Model):
     uname = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
 
-# class Track:
-#     uuid = models.CharField(max_length=50, primary_key=True)
-#     name = models.CharField(max_length=20)
-#     date = models.DateField()
-#     password = models.CharField(max_length=50)
-#
-#
-# class TrackPoint:
-#     uuid = models.CharField(max_length=50, primary_key=True)
+
+class Track(models.Model):
+    uuid = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    password = models.CharField(max_length=50,default='123456')
+
+class TrackPoint(models.Model):
+    uuid = models.CharField(max_length=50, primary_key=True)
+    latitude = models.FloatField(max_length=50)
+    longitude = models.FloatField(max_length=50)
+    accurary = models.FloatField(max_length=50)
+    date = models.DateTimeField()
+    satellite_num = models.IntegerField()
+    loc_type = models.IntegerField()
+    alt = models.FloatField(max_length=50)
+    speed = models.FloatField(max_length=50)
+
+    tk = models.ForeignKey(Track,on_delete="CASCADE")
--- Record/views/recodes.py
+++ Record/views/recodes.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from Record import models
 
 def home(request):
-    return render(request, "home.html")
+    # 查询所有轨迹
+    track_list=models.Track.objects.all()
+
+
+
+    return render(request, "home.html",locals())
 
 
 def main(req):
